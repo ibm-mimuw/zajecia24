e8082af704bdbda8fdd30cd57fc29ace2424fb0a
Mateusz Bajorek <mateusz@bajorek.it>
2025-04-24 16:23:11 +0200

    Add a test file

---
+++ test.py
@@ -0,0 +1,12 @@
+def znajdz(s, p):
+    n = len(s)
+    m = len(p)
+    ans = []
+    for i in range(n):
+        if s[i: i + m] == p:
+            ans.append(i)
+    return ans
+
+print(znajdz("ababaabaababaabaaabaabaa", "abaaba"))
+
+# ABCDEF
